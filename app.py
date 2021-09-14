49029b0e51f8d7f73878d1fad37c7120570a93a4
Pierre Thodoroff <pierthodo@gmail.com>
2021-09-14 21:14:22 +0100

    t

--- app.py
+++ app.py
@@ -26,33 +26,36 @@ def standardize_postion(dic,exchange_name):
         }
 
 
+for s in ["","_FOLLOW"]:
+    KEY = st.secrets["KEY_FTX"+s]
+    SECRET = st.secrets["SECRET_FTX"+s]
+    if s == "_FOLLOW":
+        exchange_ftx = ccxt.ftx({'apiKey':KEY,'secret':SECRET,'headers': {
+                'FTX-SUBACCOUNT': 'follownapbots',
+            }
+        })
+    else:
+        exchange_ftx = ccxt.ftx({'apiKey':KEY,'secret':SECRET,'headers': {
+                'FTX-SUBACCOUNT': 'napbots',
+            }
+        })
+    info = exchange_ftx.fetchBalance()
+    balance_value = info['info']['result'][0]['total']
+    print(balance_value)
+    positions = exchange_ftx.fetch_account_positions()
 
-KEY = st.secrets["KEY_FTX"]
-SECRET = st.secrets["SECRET_FTX"]
+    data = [standardize_postion(pos,'ftx') for pos in positions]
+    # Binance keys
+    KEY = st.secrets["KEY_BINANCE"]
+    SECRET = st.secrets["SECRET_BINANCE"]
+    exchange_binance = ccxt.binance({'apiKey':KEY,'secret':SECRET,'options': {'defaultType': 'future' }})
 
-exchange_ftx = ccxt.ftx({'apiKey':KEY,'secret':SECRET,'headers': {
-        'FTX-SUBACCOUNT': 'napbots',
-    }
-})
-info = exchange_ftx.fetchBalance()
-balance_value = info['info']['result'][0]['total']
-print(balance_value)
-positions = exchange_ftx.fetch_account_positions()
+    info = exchange_binance.fetchBalance()
+    positions = info['info']['positions']
+    for pos in positions:
+        if float(pos['positionAmt']) != 0:
+            data.append(standardize_postion(pos,'binance'))
 
-data = [standardize_postion(pos,'ftx') for pos in positions]
-data_pd = pd.DataFrame(data)
-st.write(data_pd)
-# Binance keys
-KEY = st.secrets["KEY_BINANCE"]
-SECRET = st.secrets["SECRET_BINANCE"]
-exchange_binance = ccxt.binance({'apiKey':KEY,'secret':SECRET,'options': {'defaultType': 'future' }})
-
-info = exchange_binance.fetchBalance()
-positions = info['info']['positions']
-for pos in positions:
-    if float(pos['positionAmt']) != 0:
-        data.append(standardize_postion(pos,'binance'))
-
-data_pd = pd.DataFrame(data)
-print(data_pd)
-st.write(data_pd)
+    data_pd = pd.DataFrame(data)
+    print(data_pd)
+    st.write(data_pd)
